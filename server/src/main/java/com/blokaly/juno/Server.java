2edf1c1790049892875bab67d2b9b686201c75c3
Charlie Zhang <blokaly@users.noreply.github.com>
2017-08-26 17:55:26 +0800

    serving node client files by java server

--- server/src/main/java/com/blokaly/juno/Server.java
+++ server/src/main/java/com/blokaly/juno/Server.java
@@ -7,6 +7,7 @@ public class Server {
     public static void main(String[] args) {
 
         Service service = Service.ignite().port(4567);
+        service.staticFileLocation("/public");
         service.webSocket("/juno", WebSocketHandler.class);
         service.get("/juno", (req, res) -> {
             res.status(200);
