b472e11d697735058585b8f6e825974b8d5cf1f7
Charlie Zhang <blokaly@users.noreply.github.com>
2017-08-26 15:52:27 +0800

    single thread actions

--- server/src/main/java/com/blokaly/juno/WebSocketHandler.java
+++ server/src/main/java/com/blokaly/juno/WebSocketHandler.java
@@ -1,6 +1,5 @@
 package com.blokaly.juno;
 
-import com.google.common.collect.Sets;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
@@ -10,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -20,30 +20,46 @@ public class WebSocketHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketHandler.class);
 
     private final ExecutorService es = Executors.newSingleThreadExecutor();
-    private final Set<Session> sessions = Sets.newConcurrentHashSet();
+    private final Set<Session> sessions = new HashSet<>();
 
     @OnWebSocketConnect
     public void onConnect(Session session) throws Exception {
-        LOGGER.info("WS session connected: {}", session);
-        sessions.add(session);
+        LOGGER.info("WS session connected: {}", session.getRemoteAddress());
+        es.submit(() -> {
+            addSession(session);
+        });
     }
 
     @OnWebSocketClose
     public void onClose(Session session, int statusCode, String reason) {
-        LOGGER.info("WS session disconnected: {}", session);
-        sessions.remove(session);
+        LOGGER.info("WS session disconnected: {}", session.getRemoteAddress());
+        es.submit(() -> {
+            removeSession(session);
+        });
     }
 
     @OnWebSocketMessage
     public void onMessage(Session session, String json) {
-        es.submit(()->{
-            sessions.forEach(sess->{
-                try {
-                    sess.getRemote().sendString(json);
-                } catch (IOException ex) {
-                    LOGGER.error("Failed to broadcast message to session " + sess, ex);
-                }
-            });
+        es.submit(() -> {
+            broadcast(json);
+        });
+    }
+
+    private void addSession(Session session) {
+        sessions.add(session);
+    }
+
+    private void removeSession(Session session) {
+        sessions.remove(session);
+    }
+
+    private void broadcast(String message) {
+        sessions.forEach(session -> {
+            try {
+                session.getRemote().sendString(message);
+            } catch (IOException ex) {
+                LOGGER.error("Failed to broadcast message to session " + session.getRemoteAddress(), ex);
+            }
         });
     }
 }
